f52f185851aacc7909d9de2f9646cfc38637c36f
heyuanlong <1023417614@qq.com>
2017-03-07 10:15:18 +0800

    szfkjsld

---
+++ go_fun_test/小demo/scp传输文件/scp.go
@@ -0,0 +1,19 @@
+package main  
+  
+import (  
+    "fmt"  
+    "go-ssh/ssh"  
+    "io"  
+    "os"  
+)  
+  
+const (  
+    user     = ""  
+    ip_port  = "112.112.112.112:20"  
+    password = ""  
+)  
+  
+func main() {  
+    
+}  
+
